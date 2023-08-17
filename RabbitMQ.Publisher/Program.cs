10ae1351ba38892fca4db64129018ca462cca95e
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-08-17 18:08:33 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://jridjagp:9MwWMsFzP64dKN3aapDBrjqoQmOvaiqt@fish.rmq.cloudamqp.com/jridjagp");
+
+//create connection
+using var connection = factory.CreateConnection();
+
+//create chanel
+var channel = connection.CreateModel();
+
+//create queue
+channel.QueueDeclare("hello-queue", true, false, false);
+
+//create message
+string message = "hello world";
+
+//convert message to byte
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("Message sent successfuly");
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://jridjagp:9MwWMsFzP64dKN3aapDBrjqoQmOvaiqt@fish.rmq.cloudamqp.com/jridjagp");
+
+//create connection
+using var connection = factory.CreateConnection();
+
+//create chanel
+var channel = connection.CreateModel();
+
+//already declared in publisher
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+
+    Console.WriteLine("Gelen Mesaj: {0}", message);
+};
+
+Console.ReadLine();
